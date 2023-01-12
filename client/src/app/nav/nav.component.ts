c82f865f59914f277c3b67ada7c9b9cc65259b2c
Pavel Wolfson <pzozulka@gmail.com>
2023-01-12 11:47:57 -0800

    End of section 9

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -27,6 +27,7 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe({
       next: () => this.router.navigateByUrl('/members'),
+      error: (error) => console.log(error),
     });
   }
 
